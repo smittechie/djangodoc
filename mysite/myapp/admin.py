ad73c3227b0a3e340a4e6653a2dff0f5ff3b0e2a
smittechiee <smit.patel@trootech.com>
2023-01-20 18:56:58 +0530

    many to many relationship

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Person
+from .models import Person, Group, Person1, Membership
 
 # Register your models here.
 admin.site.register(Person)
+admin.site.register(Group)
+admin.site.register(Person1)
+admin.site.register(Membership)
--- mysite/myapp/forms.py
+++ mysite/myapp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Person, Fruits
+from .models import Person, Fruits, Person1, Group, Membership
 
 
 class ShirtForm(forms.ModelForm):
@@ -12,3 +12,21 @@ class FruitsForm(forms.ModelForm):
     class Meta:
         model = Fruits
         fields = '__all__'
+
+
+class PGMPersonForm(forms.ModelForm):
+    class Meta:
+        model = Person1
+        fields = ['name']
+
+class PGMGroupForm(forms.ModelForm):
+    class Meta:
+        model = Group
+        fields = ['name']
+
+
+class PGMMemberForm(forms.ModelForm):
+    person1 = forms.CharField(max_length=60)
+    class Meta:
+        model = Membership
+        fields = ['person1', 'date_joined', 'invite_reason']
